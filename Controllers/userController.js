3061b59ddbfa984a47d56115c1d31584bf119ada
Athulsn10 <athulnair10@outlook.com>
2023-11-14 13:12:34 +0530

    login/register

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,4 +1,5 @@
 const users = require('../Models/userScheme')
+const jwt = require('jsonwebtoken')
 
 // register
 exports.register= async (req,res)=>{
@@ -20,5 +21,24 @@ exports.register= async (req,res)=>{
     }
     }catch{
         res.status(401).json(`Register API failed, Error: ${err}`)
+    }
+}
+
+// login
+exports.login = async (req, res)=>{
+    console.log('inside login controller');
+    const {email,password} = req.body
+    try{
+        const existingUser = await users.findOne({email,password})
+        if(existingUser){
+            const token = jwt.sign({userId:existingUser._id},"thegodofstories")
+            res.status(200).json({
+                existingUser,token
+            })
+        }else{
+            res.status(404).json(`Incorrect User Credentials`)
+        }
+    }catch(err){
+        res.status(401).json(`login API failed: ${err}`)
     }
 }
--- Routes/router.js
+++ Routes/router.js
@@ -5,9 +5,11 @@ const router = new express.Router()
 // import controller
 const userController = require('../Controllers/userController')
 // using router objects create routes
+
 // register API
 router.post('/user/register',userController.register)
-
+// login
+router.post('/user/login',userController.login)
 
 // export router
 module.exports = router
